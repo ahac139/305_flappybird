27be8f7ca226cf67f7251d575604824b4821f61d
Kevin <161101313+kevoutvu@users.noreply.github.com>
2025-05-14 13:58:50 +1200

    implemented text.vhd file and connected to display

--- MiniprojectResources/display.vhd
+++ MiniprojectResources/display.vhd
@@ -3,14 +3,14 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity display is
-	port(Clk, pb1, pb2 : in std_logic;
-	VGA_R, VGA_G, VGA_B : out std_logic_vector(3 downto 0) := "0000";
-	hex0, hex1, hex2 : out std_logic_vector(6 downto 0);
-	SW : in std_logic_vector(9 downto 0);
-	VGA_HS, vert_sync: out std_logic;
-	PS2_DAT, PS2_CLK : inout std_logic;
-	LEDR: out std_logic_vector(9 downto 0) := "0000000000";
-	GPIO_0 : out std_logic_vector(35 downto 0)
+	port(Clk, pb1, pb2	: in std_logic;
+	VGA_R, VGA_G, VGA_B 	: out std_logic_vector(3 downto 0) := "0000";
+	hex0, hex1, hex2 		: out std_logic_vector(6 downto 0);
+	SW 						: in std_logic_vector(9 downto 0);
+	VGA_HS, vert_sync		: out std_logic;
+	PS2_DAT, PS2_CLK 		: inout std_logic;
+	LEDR						: out std_logic_vector(9 downto 0) := "0000000000";
+	GPIO_0 					: out std_logic_vector(35 downto 0)
 	);
 end entity display;
 
@@ -18,91 +18,85 @@ end entity display;
 architecture behaviour of display is 
 
 component VGA_SYNC IS
-	PORT(	clock_25Mhz, red, green, blue		: IN	STD_LOGIC;
+	PORT(	clock_25Mhz, red, green, blue											: IN	STD_LOGIC;
 			red_out, green_out, blue_out, horiz_sync_out, vert_sync_out	: OUT	STD_LOGIC;
-			pixel_row, pixel_column: OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
+			pixel_row, pixel_column													: OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
 END component VGA_SYNC;
 
 component clock_divider is
 	port(Clk : in std_logic;
-	Clk_out : out std_logic);
+	Clk_out 	: out std_logic);
 end component clock_divider;
 
 component bouncy_ball IS
 	PORT
-		( pb1, pb2, clk, vert_sync	: IN std_logic;
-          pixel_row, pixel_column, mouse_x, mouse_y	: IN std_logic_vector(9 DOWNTO 0);
-		  ball_on : out std_logic);		
+		( 	pb1, pb2, clk, vert_sync						: IN std_logic;
+			pixel_row, pixel_column, mouse_x, mouse_y	: IN std_logic_vector(9 DOWNTO 0);
+			ball_on 												: out std_logic);		
 END component bouncy_ball;
 
 component MOUSE IS
-   PORT( clock_25Mhz, reset 		: IN std_logic;
-         mouse_data					: INOUT std_logic;
-         mouse_clk 					: INOUT std_logic;
+   PORT( clock_25Mhz, reset	 		: IN std_logic;
+         mouse_data						: INOUT std_logic;
+         mouse_clk 						: INOUT std_logic;
          left_button, right_button	: OUT std_logic;
-		 mouse_cursor_row 			: OUT std_logic_vector(9 DOWNTO 0); 
-		 mouse_cursor_column 		: OUT std_logic_vector(9 DOWNTO 0));       	
+		 mouse_cursor_row 				: OUT std_logic_vector(9 DOWNTO 0); 
+		 mouse_cursor_column 			: OUT std_logic_vector(9 DOWNTO 0));       	
 END component MOUSE;
 
 component seven_seg_display is
-     port (binary_in : in std_logic_vector(7 downto 0);
+     port (binary_in			 : in std_logic_vector(7 downto 0);
 			  hex0, hex1, hex2 : out std_logic_vector(6 downto 0));
 end component;
 
-component char_rom IS
+component text IS
 	PORT
-	(
-		character_address	:	IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-		font_row, font_col	:	IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-		clock				: 	IN STD_LOGIC ;
-		rom_mux_output		:	OUT STD_LOGIC
-	);
-END component;
+		( clk								: IN std_logic;
+        pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+		  char_on						: out std_logic);		
+END component text;
 
 
-signal pixel_row : std_logic_vector(9 downto 0);
-signal pixel_column : std_logic_vector(9 downto 0);
-signal clk_div : std_logic;
+
+signal pixel_row 		: std_logic_vector(9 downto 0);
+signal pixel_column 	: std_logic_vector(9 downto 0);
+
+signal clk_div			: std_logic;
 
 signal seven_seg_input : std_logic_vector(9 downto 0) := "0000000001";
 
-signal v_sync_i : std_logic;
+signal v_sync_i 		: std_logic;
 
-signal red : std_logic;
+signal red 				: std_logic;
+signal green 			: std_logic;
+signal blue 			: std_logic;
 
-signal green : std_logic;
+signal ground			: std_logic := '0';
 
-signal blue : std_logic;
+signal R1				: std_logic;
 
-signal ground : std_logic := '0';
+signal mouse_cursor_row		: std_logic_vector(9 DOWNTO 0); 
+signal mouse_cursor_column	: std_logic_vector(9 DOWNTO 0);
 
-signal R1: std_logic;
+signal mouse_x			: std_logic_vector(9 DOWNTO 0) := "0000000000"; 
+signal mouse_y			: std_logic_vector(9 DOWNTO 0) := "0000000000";
 
-signal mouse_cursor_row: std_logic_vector(9 DOWNTO 0); 
-signal mouse_cursor_column:std_logic_vector(9 DOWNTO 0);
+signal mouse_right	: std_logic;
+signal mouse_left 	: std_logic;
 
-signal mouse_x: std_logic_vector(9 DOWNTO 0) := "0000000000"; 
-signal mouse_y:std_logic_vector(9 DOWNTO 0) := "0000000000";
+signal switches		: std_logic_vector(7 DOWNTO 0); 
 
-signal mouse_right: std_logic;
-signal mouse_left : std_logic;
+signal char_on 		: std_logic;
+signal ball_on 		: std_logic; 
 
-signal switches: std_logic_vector(7 DOWNTO 0); 
-
-signal char_on : std_logic;
-signal ball_on : std_logic; 
-
-
-signal char_A : std_logic_vector(5 downto 0) := "000001";
 
 begin
 
-	char_display: char_rom port map(
-		character_address => char_A,
-		font_row => pixel_row(2 downto 0),
-		font_col => pixel_column(2 downto 0),
-		clock => clk_div,
-		rom_mux_output => char_on);
+	char_display: text port map(
+			clk				=> clk_div,				
+			pixel_row 		=> pixel_row,
+			pixel_column	=> pixel_column,
+			char_on			=> char_on);
 	
 	seg_display: seven_seg_display port map(
 		binary_in => switches,
@@ -143,9 +137,11 @@ begin
 	
 	-- Colours for pixel data on video signal
 	-- Changing the background and ball colour by pushbuttons
-	Red <=  ball_on or char_on;
+	Red 	<= '1' when (char_on = '1') else
+				'1' when (ball_on = '1') else
+				'0';
 	Green <= char_on;
-	Blue <= char_on;
+	Blue 	<= char_on;
 	
 	
 	
