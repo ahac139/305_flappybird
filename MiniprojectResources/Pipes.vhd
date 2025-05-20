a053791389c0b28b543d97a8c33e71695d86c487
Vision101010 <107334785+Vision101010@users.noreply.github.com>
2025-05-20 18:32:47 +1200

    Pipe will move left to right

--- MiniprojectResources/Pipes.vhd
+++ MiniprojectResources/Pipes.vhd
@@ -1,39 +1,45 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.std_logic_arith.all;
-use IEEE.std_logic_unsigned.all;
 
 entity Pipes is
-	port(clk, vert_sync: in std_logic;
-		  pixel_row, pixel_col: in std_logic_vector(9 downto 0);
-		  red, green, blue: out std_logic);
+	port(
+		clk, vert_sync: in std_logic;
+		pixel_row, pixel_col: in std_logic_vector(9 downto 0);
+		red, green, blue: out std_logic
+	);
 end Pipes;
 
 architecture behaviour of Pipes is
-	signal size_y, size_x: std_logic_vector(9 downto 0);
-	signal pipe_y_pos, pipe_x_pos, pipe_x_motion: std_logic_vector(9 downto 0);
+
+	signal size_y, size_x: unsigned(9 downto 0);
+	signal pipe_y_pos, pipe_x_pos, pipe_x_motion: unsigned(9 downto 0);
 	signal pipe_on: std_logic;
-	signal max_x, min_x: std_logic_vector(9 downto 0);
-	begin
-		
-		size_y <= CONV_STD_LOGIC_VECTOR(240, 10);
-		size_x <= CONV_STD_LOGIC_VECTOR(120, 10);
-		--pipe_y_pos <= CONV_STD_LOGIC_VECTOR(0, 10);
-		pipe_x_pos <= CONV_STD_LOGIC_VECTOR(0, 10);
-		pipe_x_motion <= CONV_STD_LOGIC_VECTOR(2, 10);
-		max_x <= CONV_STD_LOGIC_VECTOR(639, 10);
-		min_x <= CONV_STD_LOGIC_VECTOR(0, 10);
-		
-		
-		pipe_on <= '1' when (pipe_x_pos <= pixel_col) and (pixel_col <= pipe_x_pos + size_x) 
-							and (pipe_y_pos <= pixel_row) and (pixel_row <= pipe_y_pos + size_y )
-						else '0';
-						
-		red <= not pipe_on;
-		green <= '1';
-		blue <= not pipe_on;
-		
+	signal max_x, min_x: unsigned(9 downto 0);
+
+begin
+
+	-- Set constants
+	size_y <= to_unsigned(240, 10);
+	size_x <= to_unsigned(120, 10);
+	pipe_y_pos <= to_unsigned(0, 10);
+	pipe_x_motion <= to_unsigned(2, 10);
+	max_x <= to_unsigned(639, 10);
+	min_x <= to_unsigned(0, 10);
+
+	-- Pipe visibility logic
+	pipe_on <= '1' when (pipe_x_pos <= unsigned(pixel_col)) and 
+					   (unsigned(pixel_col) <= pipe_x_pos + size_x) and 
+					   (pipe_y_pos <= unsigned(pixel_row)) and 
+					   (unsigned(pixel_row) <= pipe_y_pos + size_y)
+			  else '0';
+
+	-- Color output
+	red <= not pipe_on;
+	green <= '1';
+	blue <= not pipe_on;
+
+	-- Pipe movement
 	Move_pipe: process (vert_sync)
 	begin
 		if rising_edge(vert_sync) then
@@ -44,5 +50,5 @@ architecture behaviour of Pipes is
 			end if;
 		end if;
 	end process Move_pipe;
-						
+
 end behaviour;
